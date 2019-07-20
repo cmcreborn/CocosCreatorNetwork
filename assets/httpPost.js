7ebe9d852a08fb3b0ae5d3b8eda3433fb54fc222
cmcreborn@gmail.com <cmcreborn@gmail.com>
2019-07-21 00:23:20 +0800

    [MOD] xhr status

--- assets/httpPost.js
+++ assets/httpPost.js
@@ -8,6 +8,7 @@
 //  - [Chinese] https://docs.cocos.com/creator/manual/zh/scripting/life-cycle-callbacks.html
 //  - [English] https://www.cocos2d-x.org/docs/creator/manual/en/scripting/life-cycle-callbacks.html
 
+
 cc.Class({
     extends: cc.Component,
 
@@ -38,49 +39,29 @@ cc.Class({
         logScrollViewMsg: {
             default: null,
             type: cc.Label
-        }
+        },
+        
     },
 
     // LIFE-CYCLE CALLBACKS:
 
-    // onLoad () {},
-
+    // onLoad () {
+    // },
+   
     start() {
-        console.log('start...')
+        console.log('start...');
         this.postUri.string = "http://localhost:3000/info";
+        
     },
 
     // update (dt) {},
-
-    sendXHRAB(uri) {
-        console.log("sendXHRAB... uri = " + uri);
-        var xhr = cc.loader.getXMLHttpRequest();
-        var cocosDataJsonObj = {
-            "test": "cocos creator",
-            "test2": "cocos creator",
-            "test3": {
-                "key1": "cocos creator",
-                "key2": "cocos creator"
-            }
-        }
-        xhr.open("POST", uri);
-        //set Content-type "text/plain" to post ArrayBuffer or ArrayBufferView
-        xhr.setRequestHeader("Content-Type", "application/json");
-        // Uint8Array is an ArrayBufferView
-        // xhr.send(new Uint8Array([1,2,3,4,5]));
-        console.log(cocosDataJsonObj);
-        var cocosDataJsonString = JSON.stringify(cocosDataJsonObj)
-        console.log(cocosDataJsonString)
-        xhr.send(cocosDataJsonString);
-
-    },
 
     sendXHRJ(uri) {
         console.log("sendXHRJ... uri = " + uri);
         var that = this;
         
         var xhr = cc.loader.getXMLHttpRequest();
-        var cocosDataJsonObj = { "BetInfo": { "event": true, "LineNum": "8", "LineBet": "1", "BetBalanceRate": "1", "BetCreditRate": "1", "BetCredit": 8 }, "PayTotal": 10, "BetTotal": 8, "Credit": 499992, "WagersID": "310616168187", "hasAllCards": false, "Bell3Times": 5, "Cherry3Times": 1, "Star97_A7": { "1:1": 4512.98 }, "Star97_AB": { "1:1": 4683.98 }, "Star97_AE": { "1:1": 1574.86 }, "isAllCardsJackpot": false, "hasError": false, "Cards": ["4", "6", "5", "7", "3", "2", "7", "4", "4"], "hasScatter": false, "hasFreeGame": false, "hasLine": true, "Lines": [{ "LineID": 4, "GridNum": 3, "Grids": ["1", "5", "9"], "Payoff": 10, "Element": ["4", "3", "4"], "ElementID": "4", "Grid": ["1", "5", "9"] }], "LinePayoff": 10, "isHitJackpot": false }
+        var cocosDataJsonObj = {"key":"value", "key2":{"objK":"objvalue"}, "key3":["a","2"]};
         xhr.open("POST", uri);
         //set Content-type "text/plain" to post ArrayBuffer or ArrayBufferView
         xhr.setRequestHeader("Content-Type", "application/json");
@@ -92,12 +73,10 @@ cc.Class({
         xhr.send(cocosDataJsonString);
 
         xhr.onreadystatechange = function () {
-            if (xhr.readyState == 4 && xhr.status == 200) {
-                console.log(xhr.responseText);
-                that.logScrollViewMsg.string += xhr.responseText;
-            } else {
-                console.log(xhr.statusText);
-                that.logScrollViewMsg.string += xhr.responseText;
+            if (xhr.readyState == 4 && (xhr.status >= 200 && xhr.status < 400)) {
+                var response = xhr.responseText;
+                console.log(response);
+                that.logScrollViewMsg.string += response;
             }
         };
     },
@@ -111,10 +90,11 @@ cc.Class({
 
     sendBtnTimes () {
         console.log("sendBtnTimes...");
-        var times = 1000;
+        var times = 100;
         var that = this;
         for(var i = 0; i <= times; i++){
             that.sendXHRJ(that.postUri.string);
         }
-    }
+    },
+
 });
